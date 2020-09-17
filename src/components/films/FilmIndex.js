ab5fed7ff78f907815d60adf243f4c66ac1d0555
Lewis Jones <lewisjones0000@gmail.com>
2020-09-17 16:37:06 +0100

    16:40 Commit

--- src/components/films/FilmIndex.js
+++ src/components/films/FilmIndex.js
@@ -5,20 +5,45 @@ import FilmCard from './FilmCard'
 
 class FilmIndex extends React.Component {
   state = {
-    films: null
+    films: null,
+    currentPage: 1,
+    totalPages: null,
+    onePage: 1
   }
 
   async componentDidMount() {
-    console.log('did mount')
     //get all films
-    const response = await getAllFilms()
+    const response = await getAllFilms(this.state.page)
     console.log(response)
     // if console.log works, switch on below function to setState
     //add them to state
     this.setState({
-      films: response.data.results
+      films: response.data.results,
+      page: response.data.page,
+      totalPages: response.data.totalPages
     })
   }
+
+  refreshPage = () => {
+    
+  }
+
+  handleNextPage = () => {
+    const plusOne = this.state.currentPage + this.state.onePage
+    this.setState({
+      currentPage: plusOne
+    })
+  }
+
+  handlePreviousPage = () => {
+    const minusOne = this.state.currentPage - this.state.onePage
+    this.setState({
+      currentPage: minusOne
+    })
+  }
+
+
+  //class method getfilms
 
   render() {
     console.log(this.state.films)
@@ -32,7 +57,35 @@ class FilmIndex extends React.Component {
             )) }
           </div>
         </div>
+        <nav class="pagination" role="navigation" aria-label="pagination">
+          <a onClick={this.handlePreviousPage} class="pagination-previous">Previous</a>
+          <a onClick={this.handleNextPage} class="pagination-next">Next page</a>
+          <ul class="pagination-list">
+            {/* <li>
+              <a class="pagination-link" aria-label="Goto page 1">1</a>
+            </li>
+            <li>
+              <span class="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a class="pagination-link" aria-label="Goto page 45">45</a>
+            </li> */}
+            <li>
+              <a class="pagination-link is-current" aria-label={this.state.page} aria-current="page">{this.state.page}</a>
+            </li>
+            {/* <li>
+              <a class="pagination-link" aria-label="Goto page 4">47</a>
+            </li>
+            <li>
+              <span class="pagination-ellipsis">&hellip;</span>
+            </li>
+            <li>
+              <a class="pagination-link" aria-label="Goto page 86">86</a>
+            </li>*/}
+          </ul> 
+        </nav>
       </div>
+      
     )
   }
 }
--- src/lib/api.js
+++ src/lib/api.js
@@ -1,11 +1,29 @@
 import axios from 'axios'
 
-const baseUrlDiscover = 'https://api.themoviedb.org/3/discover/movie?api_key=5adb5c242d8f7268a630ecc811e49a81'
+import FilmIndex from '../components/films/FilmIndex'
 
-export const getAllFilms = () => {
-  return axios.get(`${baseUrlDiscover}&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=1&vote_count.gte=100&vote_average.gte=8`)
+const baseUrlDiscover = 'https://api.themoviedb.org/3'
+
+
+export const getAllFilms = ( page ) => {
+  return axios.get(`${baseUrlDiscover}/discover/movie?api_key=5adb5c242d8f7268a630ecc811e49a81&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&vote_count.gte=100&vote_average.gte=8`)
 }
 
+// export const getNextPage = () => {
+//   return axios.get(`${baseUrlDiscover}/discover/movie?api_key=5adb5c242d8f7268a630ecc811e49a81&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&vote_count.gte=100&vote_average.gte=8`)
+// }
+// export const getPreviousPage = () => {
+//   return axios.get(`${baseUrlDiscover}/discover/movie?api_key=5adb5c242d8f7268a630ecc811e49a81&language=en-US&sort_by=release_date.desc&include_adult=false&include_video=false&page=${page}&vote_count.gte=100&vote_average.gte=8`)
+// }
 
-// Search Function
-// Display first 
+
+// Search Function (Base url discover+ parameters)
+// Find out how many pages in object
+// setState: Pages
+// make buttons based on number of pages
+// each button has value (1,2,3,4,5,6,7)
+// If user clicks on button, re-render filmindex changing the getAllFilm function utilising the page number value
+
+// export const searchFilms = ( page ) => {
+//   return axios.get(`${baseUrlDiscover}/search/movie?api_key=5adb5c242d8f7268a630ecc811e49a81&query=${event.target.handle}`)
+// }
