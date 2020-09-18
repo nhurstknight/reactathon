fb21419f5413e8c13109c032c12595885458e709
Lewis Jones <lewisjones0000@gmail.com>
2020-09-18 09:09:48 +0100

    Randomise Changes

--- src/components/films/Randomise.js
+++ src/components/films/Randomise.js
@@ -1,59 +1,41 @@
 import React from 'react'
 
 // import FilmShow from './common/FilmsShow'
-// import getRandomFilm from '../../lib/api'
-import { Link } from 'react-router-dom'
+import { getRandomFilm } from '../../lib/api'
+import { Redirect } from 'react-router-dom'
 
 class Randomise extends React.Component {
   state = {
-    id: 824
+    id: null
   }
 
   generateRandomNumber = () => {
-    const random = Math.floor(Math.random() * 11)
+    const random = Math.floor(Math.random() * 1100)
     console.log(random)
-
-    // const compChoice = this.choices[random]
-
-    // const result = this.setState({ playerChoice: choice })
-
-    // this.setState({ playerChoice: choice, compChoice, result })
+    this.setState({
+      id: random
+    })
   }
 
-  // async componentDidMount() {
-  //   this.generateRandomNumber()
-  //   console.log('mounting')
-  //   // make a request for the single cheese
-  //   const randomFilmId = this.state.id
-  //   const response = await getRandomFilm(randomFilmId)
-  //   console.log(response)
-  //   this.setState({
-  //     id: response.data
-  //   })
-  //   // this.fixApiGenres()
-  // }
+  async componentDidMount() {
+    this.generateRandomNumber()
+    const randomFilmId = this.state.id
+    if (this.state.id === Number) {
+      const response = await getRandomFilm(randomFilmId)
+      console.log(response)
+      this.setState({
+        id: response.data
+      })
+    }
+  }
 
   render() {
-    
-    return (
-      <div>It works</div>
-      // <Link to={`/films/${this.state.id}`}> </Link>
-    ) 
+    if (this.state.id === Number) {
+      return (
+        <Redirect to={`/films/${this.state.id}`}/>
+      ) 
+    }
   }
 }
 
 export default Randomise
-
-// <div class="container">
-//   <Link to={`/films/${id}`}>
-//   <div class="notification">
-//     This container is <strong>centered</strong> on desktop.
-//     <button class="button is-large is-fullwidth">Large</button>
-//   </div>
-//   </Link>
-// </div>
-
-// <FilmShow />
-// button
-// random film link
-// generate a FilmShow page
